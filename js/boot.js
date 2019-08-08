c4c8962d2b3e502200b3524ce108027031ef9909
Matt Hammonde <mhammonde@users.noreply.github.com>
2019-08-08 16:20:31 +0100

    Update boot.js

--- js/boot.js
+++ js/boot.js
@@ -10,6 +10,7 @@ class scnBoot extends Phaser.Scene {
   
   	create() {
 		console.log("%cStarting PhoneGuy (C) Mechanically Separated Software 2019", "color:white; background:red");
+		var loadBar = this.add.sprite(400,200,"loading");
 		this.scene.start("scnPreload");
 	}
 }
