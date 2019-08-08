00860e6b106cf4203e6aa88f6270d671bcb3e5ba
Matt Hammonde <mhammonde@users.noreply.github.com>
2019-08-08 10:57:27 +0100

    Update boot.js
    Complete code change to Phaser 3 scene system

--- js/boot.js
+++ js/boot.js
@@ -1,24 +1,19 @@
-var boot = {};
+class scnBoot extends Phaser.Scene {
 
-boot.Boot = function() {
-  this.face = null;
-};
-
-console.log(boot);
-
-boot.Boot.prototype.constructor = boot.Boot;
-
-boot.Boot.prototype = {
-	preload: function() {
-          this.game.load.image("loading","assets/loading[1].png"); 
-	},
-  
-  	create: function() {
-    		console.log("%cStarting PhoneGuy (C) Mechanically Separated Software 2019", "color:white; background:red");
-    
-		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-		this.scale.pageAlignHorizontally = true;
-		//this.scale.setScreenSize();
-		this.game.scene.start("Preload");
+	constructor () {
+		super({ key: 'sceneBoot' });
 	}
-};
+	
+	preload () {
+        	this.load.image("loading","assets/loading[1].png"); 
+	}
+  
+  	create() {
+		console.log("%cStarting PhoneGuy (C) Mechanically Separated Software 2019", "color:white; background:red");
+		this.loading = this.add.image(200,200,'loading');
+		this.input.manager.enabled = true;
+		this.input.once('pointerdown', function () {
+			this.scene.start("scenePreload");
+		}, this);
+	}
+}
