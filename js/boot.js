c732b7f2fe4c9fe9d8a6b07de595827b5de1fd84
Matt Hammonde <mhammonde@users.noreply.github.com>
2019-08-08 01:08:02 +0100

    Add files via upload

---
+++ js/boot.js
@@ -0,0 +1,22 @@
+var boot = {};
+
+boot.Boot = function() {
+  this.face = null;
+};
+
+boot.Boot.prototype.constructor = boot.Boot;
+
+boot.Boot.prototype = {
+	preload: function() {
+          this.game.load.image("loading","assets/loading[1].png"); 
+	},
+  
+  create: function() {
+    console.log("%cStarting Phreaker (c) Falsehand Games 2019", "color:white; background:red");
+    
+		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+		this.scale.pageAlignHorizontally = true;
+		//this.scale.setScreenSize();
+		this.game.scene.start("Preload");
+	}
+};
